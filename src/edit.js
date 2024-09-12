1fe7db00e4dc47ace37b2fbc406a7421975f1956
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-09-12 15:23:42 +0800

    Fetching products with default query

--- src/edit.js
+++ src/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -11,7 +11,9 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps } from "@wordpress/block-editor";
+import { useSelect } from "@wordpress/data";
+import { useMemo } from "@wordpress/element";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -19,7 +21,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import "./editor.scss";
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -30,9 +32,18 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function Edit() {
+	const products = useSelect((select) => {
+		const { getEntityRecords } = select("core");
+		return getEntityRecords("postType", "product");
+	}, []);
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Pc Workshop – hello from the editor!', 'pc-workshop' ) }
+		<p {...useBlockProps()}>
+			{products.map(({ title }) => {
+				return createBlock("core/heading", {
+					content: title,
+				});
+			})}
 		</p>
 	);
 }

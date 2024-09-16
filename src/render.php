508c13bf902ea5ed0d596a634a5a053627c92542
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-09-16 15:44:43 +0800

    Display products on the frontend

--- src/render.php
+++ src/render.php
@@ -2,7 +2,23 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$products = wc_get_products(array('limit' => 10,));
+$currency = get_woocommerce_currency_symbol( get_woocommerce_currency() );
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Pc Workshop – hello from a dynamic block!', 'pc-workshop' ); ?>
-</p>
+
+<ul class="product">
+	<?php foreach ($products as $product) {
+		$data = $product->get_data();
+	?>
+		<li class="product">
+			<article>
+				<h1><?= $data['name']; ?></h1>
+				<div class="details">
+					<span class="price"><?= $currency . $data['price']; ?></span>
+				</div>
+			</article>
+		</li>
+	<?php } ?>
+</ul>
+

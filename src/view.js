62d05edda2ca86030eb48960f65988e7beba662a
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2024-09-26 17:15:09 +0800

    Simplify the import

--- src/view.js
+++ src/view.js
@@ -1,11 +1,11 @@
 import { store, getElement } from "@wordpress/interactivity";
+import { actions } from "@wordpress/interactivity-router";
 
 store("workshop", {
 	actions: {
 		*navigate(event) {
 			event.preventDefault();
 			const { ref } = getElement();
-			const { actions } = yield import("@wordpress/interactivity-router");
 			yield actions.navigate(ref.href);
 		},
 	},

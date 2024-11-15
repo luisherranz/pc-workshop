9a6451c9c7ceed40da2d873039a5ed75bc69f3a9
Luis Herranz <luisherranz@gmail.com>
2024-11-15 12:57:01 +0100

    Move data-wp-router-region to the root

--- src/render.php
+++ src/render.php
@@ -18,41 +18,39 @@ $currency = get_woocommerce_currency_symbol( get_woocommerce_currency() );
 
 ?>
 
-<div data-wp-interactive="workshop" class="pc-workshop-block">
-	<div data-wp-router-region="workshop-loop">
-		<ul class="products display-as-<?= $attributes['layout'] ?>">
-			<?php foreach ($products as $product) {
-				$data = $product->get_data();
+<div data-wp-interactive="workshop" data-wp-router-region="workshop-loop" class="pc-workshop-block">
+	<ul class="products display-as-<?= $attributes['layout'] ?>">
+		<?php foreach ($products as $product) {
+			$data = $product->get_data();
+		?>
+			<li class="product">
+				<article>
+					<img
+						class="product__img"
+						src="<?= wp_get_attachment_url( $data['image_id'] ); ?>"
+					/>
+					<h1 class="product__name"><?= $data['name']; ?></h1>
+					<div class="product__details">
+						<span class="product__price"><?= $currency . $data['price']; ?></span>
+					</div>
+				</article>
+			</li>
+		<?php } ?>
+	</ul>
+	<nav>
+		<ul class="pagination">
+			<?php for ($i = 1; $i <= $max_num_pages; $i++) {
+				$is_active = $i === $page_number;
 			?>
-				<li class="product">
-					<article>
-						<img
-							class="product__img"
-							src="<?= wp_get_attachment_url( $data['image_id'] ); ?>"
-						/>
-						<h1 class="product__name"><?= $data['name']; ?></h1>
-						<div class="product__details">
-							<span class="product__price"><?= $currency . $data['price']; ?></span>
-						</div>
-					</article>
+				<li class="pagination__page">
+					<?php if ($is_active) : ?>
+						<span class="pagination__page--is-active"><?= $i; ?></span>
+					<?php else : ?>
+						<a class="pagination__page--link" data-wp-on--click="actions.navigate" href="?page_num=<?= $i; ?>"><?= $i; ?></a>
+					<?php endif; ?>
 				</li>
 			<?php } ?>
 		</ul>
-		<nav>
-			<ul class="pagination">
-				<?php for ($i = 1; $i <= $max_num_pages; $i++) {
-					$is_active = $i === $page_number;
-				?>
-					<li class="pagination__page">
-						<?php if ($is_active) : ?>
-							<span class="pagination__page--is-active"><?= $i; ?></span>
-						<?php else : ?>
-							<a class="pagination__page--link" data-wp-on--click="actions.navigate" href="?page_num=<?= $i; ?>"><?= $i; ?></a>
-						<?php endif; ?>
-					</li>
-				<?php } ?>
-			</ul>
-		</nav>
-	</div>
+	</nav>
 </div>
 
